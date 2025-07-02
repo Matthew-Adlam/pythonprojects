c9d9bd2dd54fb3f1193473597b735777bd21a649
Matthew Adlam <mattadlam3@gmail.com>
2025-07-02 22:17:20 +1200

    Add files via upload

---
+++ math-tools.py
@@ -0,0 +1,39 @@
+def fibonacci():
+    iterations_done = 1
+
+    first_num = int(input("Input the first number: "))
+    second_num = int(input("Input the second number: "))
+    iterations = int(input("Input the number of iterations(including the two starting numbers): "))
+
+    print(first_num)
+    print(second_num)
+
+    numbers = [first_num, second_num]
+
+    while iterations_done < iterations - 1:
+        current_num = numbers[iterations_done] + numbers[iterations_done - 1]
+        numbers.append(current_num)
+        iterations_done += 1
+
+    print(numbers)
+
+    return numbers
+
+def prime():
+    
+    
+
+def main():
+    option = ""
+    math = ["Fibonaci", "Primes", "Squares"]
+    abc = ["A","B","C"]
+    for i,s in enumerate(math):
+        print(abc[i] + ": " + s)
+    option = input("Select an math tool:")
+    
+    if(option == abc[0] or option == math[0]):
+        fibonacci()
+    
+main()
+
+
